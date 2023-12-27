389d7b012dfeb497c283c0d0a4722e725ed85026
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2023-12-27 16:03:50 -0500

    only glob dirs when loading plugins

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $config = json_decode(file_get_contents('config.json'), false, 10, JSON_THROW_ON
 if ($config->maintenance) st(503);
 $req = (object)parse_url(rtrim($_SERVER['REQUEST_URI'], '/') ?: '/'); $req->query = $_REQUEST ?? []; $req->params = [];
 $page = $config->routes->{$req->path} ?? $config->routes->{@array_values(array_filter(array_keys(get_object_vars($config->routes)), function($r) use($req) { if (preg_match('#^' . preg_replace('#:(\w+)#', '(?<$1>[\w\-]+)', $r) . '$#', $req->path, $params)) { $req->params = $params; return true; } return false; }))[0]} ?? $config->routes->{'404'} ?? st(404);
-if (file_exists('plugins')) { foreach (glob('plugins/*') as $p) include "$p/" . basename($p) . '.php'; }
+if (file_exists('plugins')) { foreach (glob('plugins/*', GLOB_ONLYDIR) as $p) include "$p/" . basename($p) . '.php'; }
 if (property_exists($page, 'file') && file_exists($page->file)) { ob_start(); include $page->file; $page->body = ob_get_clean(); }
 else if (!property_exists($page, 'body')) throw new \JerpyException('File nor body property set on route');
 if (property_exists($page, 'layout') && !($l = $page->layout)) { echo $page->body; exit; } else $l = $config->layout ?? throw new \JerpyException('No global layout defined');

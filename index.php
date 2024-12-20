7e3a1541b2885ca08df685bfce69c97c1d7983ab
ginger-tek <moormann.jeremy.tech@gmail.com>
2024-12-20 15:58:09 -0500

    refactor request object to add body() and files() methods, refactor routing logic to include HTTP 404 response for not found, handle new $meta var

--- index.php
+++ index.php
@@ -6,32 +6,63 @@
  * @license     MIT public license
  */
 
-$req = (object) [
-  'uri' => parse_url(rtrim($_SERVER['REQUEST_URI'], '/') ?: '/', PHP_URL_PATH),
-  'method' => $_SERVER['REQUEST_METHOD'],
-  'query' => (object) $_GET,
-  'params' => []
-];
-if (is_file($req->uri))
-  return false;
 set_exception_handler(function ($e) {
   ob_clean();
-  exit('<style>@media(prefers-color-scheme:dark){body{background:#333;color:white;}}</style><pre><h1>❌Unhandled Exception</h1><h2>' . get_class($e) . ": " . $e->getMessage() . "\n" . $e->getTraceAsString() . '</h2></pre>');
+  $_err = date('c') . ' | ' . $e->getMessage() . ': ' . $e->getTraceAsString() . "\n";
+  file_put_contents(__DIR__ . '/logs/' . date('Y-m-d') . '.err', $_err, FILE_APPEND);
+  exit('<style>@media(prefers-color-scheme:dark){body{padding:5em;background:#333;color:white;}}</style>
+  <pre><h1>Uh Oh!</h1><h2>An unexpected error has ocurred. Please contact the site admin.</h2></pre>');
 });
+$req = new class {
+  public string $method;
+  public string $uri;
+  public object $query;
+  public object $params;
+  function __construct()
+  {
+    $this->method = $_SERVER['REQUEST_METHOD'];
+    $this->uri = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') ?: '/';
+    $this->query = (object) $_GET;
+    $this->params = (object) [];
+  }
+  function body(string $mime = null): mixed
+  {
+    return isset($_POST) ? (object) $_POST : match ($mime) {
+      'application/json' => json_decode(file_get_contents('php://input')),
+      default => file_get_contents('php://input')
+    };
+  }
+  function files(string $field): array
+  {
+    $files = [];
+    foreach ($_FILES[$field] as $k => $vs) foreach ($vs as $i => $v) {
+        $files[$i] ??= (object) [];
+        $files[$i]->{$k} = $v;
+      }
+    return $files;
+  }
+};
 require 'config.php';
 if (isset($timezone))
   date_default_timezone_set($timezone);
 foreach ($plugins as $p)
   @include "plugins/$p/$p.php";
-$page = $routes[$req->uri] ?? array_values(array_filter($routes, function ($k) use ($req) {
-  if (preg_match('#^' . preg_replace('#:(\w+)#', '(?<$1>[\w@%&+=_-]+)', $k) . '$#', $req->uri, $params))
-    return $req->params = (object) $params;
-  return false;
-}, ARRAY_FILTER_USE_KEY))[0] ?? $routes['404'] ?? null;
+if (
+  !($res = $routes[$req->uri]
+    ?? array_values(array_filter($routes, function ($k) use (&$req) {
+      if (!preg_match('#^' . preg_replace('#:(\w+)#', '(?<$1>[\w\@\#\%\&\+\=\_\-]+)', $k) . '$#', $req->uri, $params))
+        return false;
+      $req->params = (object) $params;
+      return true;
+    }, ARRAY_FILTER_USE_KEY))[0]
+    ?? null)
+) {
+  http_response_code(404);
+  $res = end($routes);
+}
 ob_start();
-if ($page === null)
-  return http_response_code(404);
-if (is_array($page))
-  extract($page);
+(is_array($res) ? extract($res) : $page = $res);
+$page = "pages/$page";
+$meta ??= [];
 include $layout === false ? $page : "layouts/$layout.php";
 ob_end_flush();

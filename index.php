2f6923ceb25952b01f65e9b7f8bbd616d4e2eff1
jmoormann.sbgtv <jmoormann.sbgtv@gmail.com>
2024-12-24 22:45:39 +0000

    reducing concerns from last version

--- index.php
+++ index.php
@@ -6,63 +6,22 @@
  * @license     MIT public license
  */
 
-set_exception_handler(function ($e) {
-  ob_clean();
-  $_err = date('c') . ' | ' . $e->getMessage() . ': ' . $e->getTraceAsString() . "\n";
-  file_put_contents(__DIR__ . '/logs/' . date('Y-m-d') . '.err', $_err, FILE_APPEND);
-  exit('<style>@media(prefers-color-scheme:dark){body{padding:5em;background:#333;color:white;}}</style>
-  <pre><h1>Uh Oh!</h1><h2>An unexpected error has ocurred. Please contact the site admin.</h2></pre>');
-});
-$req = new class {
-  public string $method;
-  public string $uri;
-  public object $query;
-  public object $params;
-  function __construct()
-  {
-    $this->method = $_SERVER['REQUEST_METHOD'];
-    $this->uri = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') ?: '/';
-    $this->query = (object) $_GET;
-    $this->params = (object) [];
-  }
-  function body(string $mime = null): mixed
-  {
-    return isset($_POST) ? (object) $_POST : match ($mime) {
-      'application/json' => json_decode(file_get_contents('php://input')),
-      default => file_get_contents('php://input')
-    };
-  }
-  function files(string $field): array
-  {
-    $files = [];
-    foreach ($_FILES[$field] as $k => $vs) foreach ($vs as $i => $v) {
-        $files[$i] ??= (object) [];
-        $files[$i]->{$k} = $v;
-      }
-    return $files;
-  }
-};
-require 'config.php';
-if (isset($timezone))
-  date_default_timezone_set($timezone);
-foreach ($plugins as $p)
-  @include "plugins/$p/$p.php";
-if (
-  !($res = $routes[$req->uri]
-    ?? array_values(array_filter($routes, function ($k) use (&$req) {
-      if (!preg_match('#^' . preg_replace('#:(\w+)#', '(?<$1>[\w\@\#\%\&\+\=\_\-]+)', $k) . '$#', $req->uri, $params))
-        return false;
-      $req->params = (object) $params;
-      return true;
-    }, ARRAY_FILTER_USE_KEY))[0]
-    ?? null)
-) {
-  http_response_code(404);
-  $res = end($routes);
-}
-ob_start();
-(is_array($res) ? extract($res) : $page = $res);
-$page = "pages/$page";
-$meta ??= [];
-include $layout === false ? $page : "layouts/$layout.php";
-ob_end_flush();
+ set_exception_handler(fn() => exit('<style>body{font-family:monospace;color:#fff;font-size:2em;background:#444;padding:5em;text-align:center}</style>
+ <p>⚠<br>Something went wrong while showing this page<br>Contact the site admin for assistance</p>'));
+ require 'config.php';
+ date_default_timezone_set($timezone ?? 'America/New_York');
+ $uri = parse_url(rtrim($_SERVER['REQUEST_URI'], '/') ?: '/', PHP_URL_PATH);
+ $params = [];
+ array_map(fn($p) => @include "plugins/$p/$p.php", $plugins);
+ if (
+   !($res = $routes[$uri] ?? current(array_filter($routes, function ($k) use (&$params, $uri) {
+     return preg_match('#^' . preg_replace('#:(\w+)#', '(?<$1>[\w\@\#\%\&\+\=\_\-]+)', $k) . '$#', $uri, $m)
+       ? $params = $m : false; }, ARRAY_FILTER_USE_KEY)) ?? false)
+ )
+   [http_response_code(404), $res = end($routes)];
+ ob_start();
+ (is_array($res) ? extract($res) : $page = $res);
+ $page = "pages/$page";
+ $meta ??= [];
+ include $layout === false ? $page : "layouts/$layout.php";
+ ob_end_flush();

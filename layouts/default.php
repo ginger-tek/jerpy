f0384fa748ff7e47f641f2185da674fd440fe466
ginger-tek <moormann.jeremy.tech@gmail.com>
2025-08-03 11:09:35 -0400

    use null coalescing

--- layouts/default.php
+++ layouts/default.php
@@ -3,11 +3,11 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>My Site - <?= @$meta['title'] ?></title>
+  <title>My Site - <?= $meta['title'] ?? '' ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="og:title" content="<?= @$meta['title'] ?>">
-  <meta name="og:description" content="<?= @$meta['description'] ?>">
-  <meta name="og:image" content="<?= @$meta['thumbnail'] ?>">
+  <meta name="og:title" content="<?= $meta['title'] ?? '' ?>">
+  <meta name="og:description" content="<?= $meta['description'] ?? '' ?>">
+  <meta name="og:image" content="<?= $meta['thumbnail'] ?? '' ?>">
   <link rel="stylesheet" href="/assets/styles.css">
 </head>
 

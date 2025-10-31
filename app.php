d5554f50888c00b774df155d884cce60c2f8791a
ginger-tek <moormann.jeremy.tech@gmail.com>
2025-10-31 00:22:38 -0400

    rework to finally serve site from public/ dir for better security, added default robots.txt, updated local dev config

--- app.php
+++ app.php
@@ -6,19 +6,20 @@
  * @license     MIT public license
  */
 
+define('ROOT', __DIR__);
 set_exception_handler(function (Throwable $ex) {
   ob_clean();
   error_log("[" . date('r') . "] {$ex->getMessage()}: {$ex->getTraceAsString()}");
   exit('<style>body{font-family:monospace;color:#fff;font-size:2em;background:#444;padding:5em;text-align:center}</style>
   <p>⚠<br>Something went wrong while showing this page<br>Contact the site admin for assistance</p>');
 });
-require 'config.php';
+require ROOT . '/config.php';
 date_default_timezone_set($timezone ?? 'America/New_York');
 $uri = parse_url(rtrim($_SERVER['REQUEST_URI'], '/') ?: '/', PHP_URL_PATH);
 $params = [];
 foreach ($plugins as $r => $p)
   if ((is_string($r) && str_starts_with($uri, $r) && $p) || (!is_string($r) && $p))
-    @include "plugins/$p/$p.php";
+    @include ROOT . "/plugins/$p/$p.php";
 if (
   !($res = $routes[$uri] ?? current(array_filter($routes, function ($k) use (&$params, $uri) {
     return preg_match('#^' . preg_replace('#:(\w+)#', '(?<$1>[\w\@\#\%\&\+\=\_\-]+)', $k) . '$#', $uri, $m)
@@ -27,7 +28,7 @@ if (
   [http_response_code(404), $res = end($routes)];
 ob_start();
 (is_array($res) ? extract($res) : $page = $res);
-$page = "pages/$page";
+$page = ROOT . "/pages/$page";
 $meta ??= [];
-include $layout === false ? $page : "layouts/$layout.php";
+require $layout === false ? $page : ROOT . "/layouts/$layout.php";
 ob_end_flush();
